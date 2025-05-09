37816945b9a6c3ed7231dfcabb126c521fe6ccdb
Duc Hien <2001215763@hufi.edu.vn>
2025-05-10 02:05:09 +0700

    feature_hien/auth_part_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:pizza_app/app.dart';
+import 'package:pizza_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
 
-void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // đảm bảo mọi thứ đều giống như khởi tạo
+  await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver(); // observe state on the bloc
+  runApp(MyApp(FirebaseUserRepo()));
 }
