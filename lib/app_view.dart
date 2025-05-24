e743327cacadb6aa39a1d0a18fc73b58774f7a4a
Duc Hien <2001215763@hufi.edu.vn>
2025-05-24 14:14:40 +0700

    feature_hien/pizza_repository

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:pizza_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
+import 'package:pizza_app/screens/home/blocs/get_pizza_bloc/get_pizza_bloc.dart';
+import 'package:pizza_repository/pizza_repository.dart';
 
 import 'screens/auth/views/welcome_screen.dart';
 import 'screens/home/views/home_screen.dart';
@@ -25,10 +27,19 @@ class MyAppView extends StatelessWidget {
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: (context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return BlocProvider(
-              create: (context) => SignInBloc(
-                context.read<AuthenticationBloc>().userRepository
+            return MultiBlocProvider(
+              providers: [
+                BlocProvider(
+                  create:
+                      (context) => SignInBloc(
+                        context.read<AuthenticationBloc>().userRepository,
+                      ),
+                ),
+                BlocProvider(create: (context) => GetPizzaBloc(
+                  FirebasePizzaRepo()
+                )..add(GetPizza())
               ),
+              ],
               child: const HomeScreen(),
             );
           } else {
